0e05dfb85be4426dec0060badc395670feb1ae27
FadhilahNurEryani <a710230078@student.ums.ac.id>
2024-06-09 16:55:19 +0700

    akar pangkat

---
+++ Day 4/Akar.py
@@ -0,0 +1,6 @@
+print("Menghitung akar pangkat tiga")
+#mengambil inputan bilangan
+#x merupakan bilangan yang akan dijadikan akar pangkat 3
+x=int(input("masukkan angka:"))
+hasil=x**(1/3)
+print(hasil)

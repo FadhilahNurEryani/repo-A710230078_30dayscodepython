47d6002b96624cf89936e985ef6f04f354c54c7d
FadhilahNurEryani <a710230078@student.ums.ac.id>
2024-06-10 07:02:15 +0700

    if elif else

---
+++ Day 5/kondisional.py
@@ -0,0 +1,8 @@
+umur = int(input("Masukkan umur anda: "))
+
+if umur < 16:
+      print("Kamu masih anak anak.")
+elif umur >= 60:
+      print("Kamu sudah lanjut usia.")
+else:
+      print("Kamu sudah dewasa.")

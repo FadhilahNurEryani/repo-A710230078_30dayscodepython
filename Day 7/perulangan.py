8ca525d20dde265e6bbac020c503ca0389baa6cd
FadhilahNurEryani <a710230078@student.ums.ac.id>
2024-06-12 06:16:36 +0700

    perulangan

---
+++ Day 7/perulangan.py
@@ -0,0 +1,11 @@
+# menggunakan perulangan for
+print("Berikut hasil menggunakan perulangan for:")
+for i in range(1, 6):
+    print(i)
+
+# menggunakan perulangan while
+print("Berikut hasil menggunakan perulangan while:")
+i = 1
+while i <= 5:
+    print(i)
+    i += 1

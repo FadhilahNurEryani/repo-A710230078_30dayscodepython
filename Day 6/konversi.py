21b15c10a5f2198d709918f03097e47b17e527b2
FadhilahNurEryani <a710230078@student.ums.ac.id>
2024-06-11 07:35:15 +0700

    convert

---
+++ Day 6/konversi.py
@@ -0,0 +1,7 @@
+print('Mengkonversi satuan jarak Kilometer menjadi Meter ')
+#Mengambil inputan kilometer
+angka=int(input("Masukkan angka yang akan dikonversi: ",))
+
+#Memasukkan rumus perhitungan
+hasil=angka*(1000)
+print("hasil konversi jarak adalah:",hasil,'m')

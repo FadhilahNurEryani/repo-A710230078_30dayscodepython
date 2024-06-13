2b9e0d6db57abf4f13e9f0e0960646e954e0d54b
FadhilahNurEryani <a710230078@student.ums.ac.id>
2024-06-13 10:59:16 +0700

    llist

---
+++ Day 8/list.py
@@ -0,0 +1,13 @@
+warna = ["Merah","Kuning","Hijau","Biru"]
+#menampilkan list dan length
+print("Warna: ",warna)
+print("Panjang: ", len(warna))
+
+(warna[3])#menampilkan warna indeks ke 3
+print(warna)
+warna.remove("Kuning")#menghapus warna kuning
+print(warna)
+warna.append("Ungu")#menambahkan warna ungu di akhir list
+print(warna)
+warna.insert(1, "Kuning")#menambahkan warna kuning di indeks 1
+print(warna)

00786374d3d0506d5f32d3fa641adb6f65ba72d9
FadhilahNurEryani <a710230078@student.ums.ac.id>
2024-06-14 15:59:24 +0700

    tuple

---
+++ Day 9/tuple.py
@@ -0,0 +1,10 @@
+#membuat tuple
+my_tuple = (1, 2, 3, 4, 5)
+print(my_tuple[0])  #print index ke 0
+print(my_tuple[4])  #print index ke 4
+print(len(my_tuple))  #menghitung panjang tuple
+print(my_tuple)
+
+my_tuple2 = ("a", "b", "c")
+print(my_tuple2)
+print(my_tuple + my_tuple2)

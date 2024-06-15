0e14e50892f8e090f80b1ed04c13872e382b8fb7
FadhilahNurEryani <a710230078@student.ums.ac.id>
2024-06-15 07:42:06 +0700

    dictionary

---
+++ Day 10/dictionary.py
@@ -0,0 +1,15 @@
+dictionary = {
+    1: 'A',
+    2: 'B',
+    3: 'C',
+    4: 'D',
+    5: 'E',
+    6: 'F',
+    7: 'G',
+    8: 'H',
+    9: 'I',
+    10: 'J'
+}
+
+print(dictionary[5])
+print(dictionary)
